252458785804de2afe95dfdc285b886d3e407864
Ashish Patel <ashishpatel3009@gmail.com>
2024-01-04 15:57:39 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Add 1 to a number represented as linked list/add-1-to-a-number-represented-as-linked-list.java
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+class GfG
+{
+    public static void printList(Node node) 
+    { 
+        while (node != null)
+        { 
+            System.out.print(node.data);
+            node = node.next; 
+        }  
+        System.out.println();
+    } 
+    public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Node head = new Node( s.charAt(0) - '0' );
+                    Node tail = head;
+                    for(int i=1; i<s.length(); i++)
+                    {
+                        tail.next = new Node( s.charAt(i) - '0' );
+                        tail = tail.next;
+                    }
+                    Solution obj = new Solution();
+                    head = obj.addOne(head);
+                    printList(head); 
+                }
+        }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+} 
+*/
+
+class Solution
+{
+    public static Node addOne(Node head) 
+    { 
+        if (head == null) {
+            return new Node(1);  // If the linked list is empty, return a new node with value 1.
+        }
+
+        // Reverse the linked list
+        head = reverseList(head);
+
+        Node current = head;
+        int carry = 1;
+
+        while (current != null) {
+            int sum = current.data + carry;
+            current.data = sum % 10;
+            carry = sum / 10;
+
+            if (current.next == null && carry > 0) {
+                current.next = new Node(carry);
+                break;
+            }
+
+            current = current.next;
+        }
+
+        // Reverse the linked list back to its original order
+        head = reverseList(head);
+
+        return head;
+    }
+
+    // Helper function to reverse a linked list
+    private static Node reverseList(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
