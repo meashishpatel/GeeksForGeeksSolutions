704a8e9094bba2b4ae9cd3444779205a3b21f383
Ashish Patel <ashishpatel3009@gmail.com>
2024-01-23 22:32:21 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Interleave the First Half of the Queue with Second Half/interleave-the-first-half-of-the-queue-with-second-half.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[] a = IntArray.input(br, N);
+            Queue<Integer> q=new LinkedList<>();
+            for(int i:a)q.add(i);
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.rearrangeQueue(N, q);
+            
+            IntArray.print(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static ArrayList<Integer> rearrangeQueue(int N, Queue<Integer> q) {
+        // code here
+        Queue<Integer> q2= new LinkedList<>();
+        ArrayList<Integer> al = new ArrayList<>();
+        
+        for (int i = 0; i < N/2; i++){
+            q2.add(q.remove());
+        }
+        
+        while (!q.isEmpty() && !q2.isEmpty()){
+            al.add(q2.remove());
+            al.add(q.remove());
+        }
+        
+        return al;
+    }
+}
+        
